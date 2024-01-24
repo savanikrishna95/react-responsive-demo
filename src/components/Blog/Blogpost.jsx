1d8218d66015683bde8e8e6ad29f0fe910f8b0b3
savanikrishna95 <savanitina@outlook.com>
2024-01-24 14:27:50 +0530

    added code

--- src/components/Blog/Blogpost.jsx
+++ src/components/Blog/Blogpost.jsx
@@ -68,7 +68,7 @@ const Wrapper = styled.section`
     color: ${({ theme }) => theme.colors.bg};
     display: flex;
     align-items: center;
-    text-align:center;
+    text-align:left;
     gap: 0.5rem;
     font-size: 1.5rem;
     cursor: pointer;
