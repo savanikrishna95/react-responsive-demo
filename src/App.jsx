c0d4aea9f5cb07bc79376924774b27860df94722
savanikrishna95 <savanitina@outlook.com>
2024-01-25 11:53:44 +0530

    added code

--- src/App.jsx
+++ src/App.jsx
@@ -46,7 +46,7 @@ const App = () => {
       currentVersion={version}
       isEnabled={isProduction} //If false, the library is disabled.
       isVerboseMode={false} //If true, the library writes verbose logs to console.
-      loadingComponent={<h1>Loading..</h1 >} //If not pass, nothing appears at the time of new version check.
+      loadingComponent={<h1>refreshing..</h1 >} //If not pass, nothing appears at the time of new version check.
       metaFileDirectory={'.'} //If public assets are hosted somewhere other than root on your server.
     >
     <ThemeProvider theme={theme}>
