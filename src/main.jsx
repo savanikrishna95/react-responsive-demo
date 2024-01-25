a308322254482cc3be2946060418c0941d2af344
savanikrishna95 <savanitina@outlook.com>
2024-01-25 15:01:22 +0530

    added code

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+
+// navigator.serviceWorker.addEventListener('message', event => {
+//   if (event.data === 'reload') {
+//     window.location.reload(true);
+//   }
+// });
+
+
+// if ('serviceWorker' in navigator) {
+//   navigator.serviceWorker
+//     .register('../public/serviceWorker.js')
+//     .then(registration => {
+//       console.log('Service Worker registered with scope:', registration.scope);
+//     })
+//     .catch(error => {
+//       console.error('Service Worker registration failed:', error);
+//     });
+// }
+
+
+serviceWorker.unregister();
